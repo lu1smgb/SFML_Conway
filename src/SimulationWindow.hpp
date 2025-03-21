9c719dda8e2c3efa8b2a5a9493530da892a8c1c2
lu1smgb <lu1smgb.gacc@gmail.com>
2025-03-21 00:40:46 +0000

    Changed render parameters

--- src/SimulationWindow.hpp
+++ src/SimulationWindow.hpp
@@ -8,8 +8,8 @@ class SimulationWindow {
 protected:
     Simulator simulator;
     sf::Vector2f size;
-    unsigned long speed = 6;
-    float renderScale = 4;
+    unsigned long speed = 60;
+    float renderScale = 8;
     bool focus = true;
     std::string name = "SimulationWindow";
 public:
